c90fd007fd9ed31f38c8b8f8762364cc0e0f2936
limchard <jksy6458@naver.com>
2024-02-08 00:31:04 +0900

    회원가입 폼 만드는중

--- react-jumprun/src/member/SignUpForm.js
+++ react-jumprun/src/member/SignUpForm.js
@@ -1,32 +1,66 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {useNavigate} from "react-router-dom";
 import('./SignUp.css');
 
 function SignUpForm(props) {
+
+    const navi=useNavigate();
+    const [data,setData]=useState({
+        id:'',
+        pw:'',
+        name:'',
+        hp:'',
+        email:'',
+        addr:'',
+        photo:'',
+        rating:'',
+        emailOk:false
+    });
+
+
+
     return (
         <div className={'signup-box'}>
-            <h2>회원가입</h2>
-            <div className={'signup-id'}>
-                <input type={"text"} placeholder={'아이디'}/>
-            </div>
-            <div className={'pw-box'}>
-                <input type={"password"} placeholder={'비밀번호'}/>
-            </div>
-            <div>
-                <input type={"text"} placeholder={'이름'}/>
-            </div>
-            <div>
-                <input type={"text"} placeholder={'연락처'}/>
-            </div>
-            <div>
-                <input type={"text"} placeholder={'이메일'}/>
-            </div>
-            <div>
-                <input type={"text"} placeholder={'주소'}/>
-            </div>
-            <div>
-                <input type={"text"} placeholder={'사진'}/>
-            </div>
-
+            <form onSubmit={"login"}>
+                <div className={'signup-title'}>회원가입</div>
+                <hr/>
+                <div className={'signup-id'}>
+                    <div className={'signup-name'}>아이디</div>
+                    <input type={"text"} className={'signup-input'} placeholder={'아이디'}/>
+                </div>
+                <div className={'pw-box'}>
+                    <div className={'signup-name'}>비밀번호</div>
+                    <input type={"password"} placeholder={'비밀번호'}/>
+                </div>
+                <div>
+                    <div className={'signup-name'}>이름</div>
+                    <input type={"text"} placeholder={'이름'}/>
+                </div>
+                <div>
+                    <div className={'signup-name'}>연락처</div>
+                    <input type={"text"} placeholder={'연락처'}/>
+                </div>
+                <div>
+                    <div className={'signup-name'}>이메일</div>
+                    <input type={"text"} placeholder={'이메일'}/>
+                </div>
+                <div>
+                    <div className={'signup-name'}>주소</div>
+                    <input type={"text"} placeholder={'주소'}/>
+                </div>
+                <div>
+                    <div className={'signup-name'}>생년월일</div>
+                    <input type={"text"} placeholder={'생년월일'}/>
+                </div>
+                <div>
+                    <div className={'signup-name'}>성별</div>
+                    <input type={"text"} placeholder={'성별'}/>
+                </div>
+                <div>
+                    <div className={'signup-name'}>사진</div>
+                    <input type={"text"} placeholder={'사진'}/>
+                </div>
+            </form>
         </div>
     );
 }
